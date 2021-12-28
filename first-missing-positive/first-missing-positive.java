2c4bc049b4c2ee797118065588c25853035cad09
anmol-Git <66984935+anmol-Git@users.noreply.github.com>
2021-12-28 16:14:31 +0530

    Time: 4 ms (44.58%), Space: 96.4 MB (71.73%) - LeetHub

---
+++ first-missing-positive/first-missing-positive.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        cyclic(nums);
+        for(int i =0;i<nums.length;i++){
+            if(nums[i] != i+1) return i+1;
+        }
+        return nums.length+1;
+    }
+    
+    void cyclic(int[] nums){
+        int i=0;
+        while(i<nums.length){
+            int correct = nums[i]-1;
+            if(nums[i] >0 && nums[i] <= nums.length && nums[correct] != nums[i]) swap(nums,i,correct);
+            else i++;
+        }
+    }
+    
+    void swap(int [] nums,int first,int second){
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
+    }
+}
