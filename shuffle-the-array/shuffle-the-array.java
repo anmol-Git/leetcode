5295d6293e9e310ef7e75184b3a061ac5c616ee7
anmol-Git <66984935+anmol-Git@users.noreply.github.com>
2021-12-29 11:52:34 +0530

    Time: 1 ms (18.74%), Space: 42.8 MB (11.65%) - LeetHub

--- shuffle-the-array/shuffle-the-array.java
+++ shuffle-the-array/shuffle-the-array.java
@@ -1,16 +1,16 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        int even = 0;
-        int odd = 1;
-        int i =0;
-        int[] ans = new int[2*n];
-        while(i < n){
-            ans[even] = nums[i];
-            ans[odd] = nums[i+n];
-            even+=2;
-            odd+=2;
-            i++;
+        
+        for(int i =n;i<nums.length;i++){
+            nums[i] =  (nums[i]*1030) + nums[i-n];
         }
-        return ans;
+        
+        int index=0;
+        for(int i =n;i<nums.length;i++){
+            nums[index] = nums[i]%1030;
+            nums[index+1] = nums[i]/1030;
+            index+=2;
+        }
+        return nums;
     }
 }
