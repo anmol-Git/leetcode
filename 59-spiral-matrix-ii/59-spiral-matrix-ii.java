c4477b062e70a67439476a73d5d82e810c43b7f1
anmol-Git <66984935+anmol-Git@users.noreply.github.com>
2022-01-04 11:57:52 +0530

    Time: 0 ms (100.00%), Space: 36.8 MB (88.95%) - LeetHub

---
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] mat = new int[n][n];
+        
+        int counter = 1;
+        
+        for(int i =1;i<=n*n;i++){
+            mat[(i-1)/n][(i-1)%n] = i;
+        }
+        
+        int uRow = 0;
+        int rCol = n-1;
+        int dRow = n-1;
+        int lCol = 0;
+        
+        while(lCol<=rCol && uRow<=dRow){
+            
+            for(int i =lCol;i<=rCol;i++){
+                mat[uRow][i] = counter++;
+            }
+            uRow++;
+            
+            for(int i =uRow;i<=dRow;i++){
+                mat[i][rCol] = counter++;
+            }
+            rCol--;
+            
+            if(uRow>dRow || rCol<lCol) return mat;
+            
+            for(int i =rCol;i>=lCol;i--){
+             mat[dRow][i] =counter++;
+            }
+            dRow--;
+            
+            for(int i =dRow;i>=uRow;i--){
+                mat[i][lCol] = counter++;
+            }
+            lCol++;
+        }
+        
+        return mat;
+    }
+}
