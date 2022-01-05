add963cc6988d7f43c120bf50b48ab28bb0828b2
anmol-Git <66984935+anmol-Git@users.noreply.github.com>
2022-01-05 16:08:55 +0530

    Time: 6 ms (6.45%), Space: 50.7 MB (15.96%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+   //     int[][] arr = Arrays.stream(matrix).map(int[]::clone).toArray(int[][]::new);
+        
+        int row = matrix.length;
+        int col = matrix[0].length;
+        
+        int [] rowArr = new int[row];
+        int [] colArr = new int[col];
+        for(int i =0;i<row;i++){
+            for(int j =0;j<col;j++){
+                if(matrix[i][j] == 0) {
+                    rowArr[i] =1;
+                    colArr[j] =1;
+                }
+            }
+        }
+        for(int i =0;i<row;i++){
+            for(int j =0;j<col;j++){
+                
+                if(rowArr[i] == 1 && colArr[j] == 1){
+                    for(int k=0;k<col;k++) matrix[i][k]=0;
+                    for(int m =0;m<row;m++) matrix[m][j]=0;
+                }   
+            }
+        } 
+    }
+}
