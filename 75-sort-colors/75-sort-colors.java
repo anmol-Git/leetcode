110d7cf52105b3cd9f92f47e0cd2551027579d39
anmol-Git <66984935+anmol-Git@users.noreply.github.com>
2022-01-07 09:54:14 +0530

    Time: 0 ms (100.00%), Space: 37.7 MB (49.13%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,28 +1,25 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int ones = 0;
-        int twoes = 0;
-        int zeroes = 0;
-        int i = 0;
-        for(i=0;i<nums.length;i++){
-            if(nums[i] == 0) zeroes++;
-            else if(nums[i] == 2) twoes++;
-            else ones++;
+        if(nums.length == 1) return;
+        int start = 0;
+        int end = nums.length-1;
+        int i =0;
+        while(i<=end){
+            if(nums[i] == 0){
+                swap(nums,i,start);
+                start++;
+            }else if(nums[i] == 2){
+                swap(nums,i,end);
+                end--;
+                i--;
+            }
+            i++;
         }
-        i = 0;
-        while(zeroes>0){
-            nums[i++] = 0; 
-            zeroes--;
-        }
-        
-        while(ones>0){
-        nums[i++] = 1;
-            ones--;
-        }
-        
-        while(twoes>0){
-            nums[i++] = 2;
-            twoes--;
-        }
+    }
+    
+    void swap(int[] nums,int first,int second){
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
     }
 }
