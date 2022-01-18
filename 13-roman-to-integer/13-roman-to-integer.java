0387dc491577f3f82d23e450a1e3d469e8e878ab
anmol-Git <66984935+anmol-Git@users.noreply.github.com>
2022-01-18 11:53:16 +0530

    Time: 4 ms (83.13%), Space: 39.9 MB (48.19%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int romanToInt(String s) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+        
+        int ans = 0;
+        int i = 0;
+        for(i = 0; i < s.length()-1; i++){
+            char curr = s.charAt(i);
+            char next = s.charAt(i+1);
+            
+            if(curr == 'I' && (next == 'V' || next == 'X')){
+                 ans -=1;
+            }else if(curr == 'X'  && (next == 'L' || next == 'C')){
+               ans-=10;
+            }else if(curr == 'C'  && (next == 'D' || next == 'M')){
+               ans-=100;
+            }
+            else ans+=map.get(curr);
+        }
+        ans += map.get(s.charAt(i));
+        return ans;
+    }
+}
