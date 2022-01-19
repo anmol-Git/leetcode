c04a63ff520b26de360b21ca11a2a2551e065467
anmol-Git <66984935+anmol-Git@users.noreply.github.com>
2022-01-19 15:55:13 +0530

    Time: 13 ms (70.01%), Space: 48.2 MB (57.04%) - LeetHub

---
+++ 1871-jump-game-vii/1871-jump-game-vii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean canReach(String s, int minJump, int maxJump) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(0);
+        for(int i =0;i<s.length();i++){
+            
+            if(s.charAt(i) == '0'){
+                
+                while(!q.isEmpty() && q.peek() < i - maxJump) q.poll();
+                
+                if(q.isEmpty()) return false;
+                
+                if(q.peek() + minJump <= i && q.peek() + maxJump >= i){
+                    q.add(i);
+                    if(i == s.length() -1) return true;
+                }
+            }
+        }
+        return false;
+    }
+}
