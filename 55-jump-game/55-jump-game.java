2713334c2e1af7b286ba112e5424e0b68f97c6ac
anmol-Git <66984935+anmol-Git@users.noreply.github.com>
2022-07-03 11:58:57 +0530

    Time: 3 ms (64.29%), Space: 65.9 MB (81.95%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,11 +1,18 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        if(nums.length ==1) return true;
-        int index = nums.length-1;
+        int i = nums.length-1;
         
-        for(int i = index;i>=0;i--){
-            if(nums[i]+ i >=index) index = i;
+        while(i>0) {
+            int j = i-1;
+            while(j>=0) {
+                if(j+nums[j] >= i) {
+                    i = j;
+                    break;
+                }
+                j--;
+            }
+            if(j == -1) return false;
         }
-        return index == 0;
+        return true;
     }
 }
