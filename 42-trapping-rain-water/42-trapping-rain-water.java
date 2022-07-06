887e549cecb354760395c799467fab09a88d104a
anmol-Git <66984935+anmol-Git@users.noreply.github.com>
2022-07-06 09:12:30 +0530

    Time: 1 ms (99.33%), Space: 48.4 MB (56.92%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,23 +1,24 @@
 class Solution {
     public int trap(int[] height) {
         int len = height.length;
-        int[] Lmax = new int[len];
-        int[] Rmax = new int[len];
-        
-        Lmax[0] = height[0];
-        Rmax[len - 1] = height[len - 1];
-        
-        for (int i = 1; i < len; i++) {
-            Lmax[i] = Math.max(Lmax[i-1], height[i]);
-            Rmax[len - i - 1] = Math.max(Rmax[len-i], height[len-i-1]);
-        }
-        
+        int start = 0;
+        int end = len - 1;
+        int maxLeft = height[start];
+        int maxRight = height[end];
         int totalWater = 0;
         
-        for(int i = 0; i < len ; i++) {
-            totalWater += Math.min(Lmax[i],Rmax[i]) - height[i];    
+        while(start < end) {
+            if(maxLeft < maxRight) {
+                start++;
+                maxLeft = Math.max(maxLeft, height[start]);
+                totalWater += maxLeft - height[start];
+            }
+            else {
+                end--;
+                maxRight = Math.max(maxRight, height[end]);
+                totalWater += maxRight - height[end];
+            }
         }
-        
         return totalWater;
     }
 }
