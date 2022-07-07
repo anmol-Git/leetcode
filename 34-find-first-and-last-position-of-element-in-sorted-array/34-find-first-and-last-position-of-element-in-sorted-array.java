e8186bf3781c059acf7a705f875d8ea2442ea305
anmol-Git <66984935+anmol-Git@users.noreply.github.com>
2022-07-07 15:24:09 +0530

    Time: 1 ms (37.58%), Space: 47.5 MB (31.38%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,31 +1,49 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        if(nums.length<1) return new int[]{-1,-1};
-        int first = firstPosition(nums,target);
-        int second = secondPosition(nums,target);
-        if(first >= nums.length || first == -1 || nums[first] != target)  return new int[]{-1,-1};         
-        return new int[]{first,second};
+          
+          int len = nums.length;
+            
+          if(len == 0) return new int[]{-1,-1}; 
+        
+          int startIndex = 0;
+          int lastIndex = 0;
+          
+           if(target < nums[0]) startIndex = -1;  
+          else startIndex = startIndex(nums, target, 0, len - 1);
+          
+          if(target > nums[len - 1]) lastIndex = -1;
+          else lastIndex = lastIndex(nums, target, 0, len - 1);
+        
+          return new int[]{startIndex,lastIndex};
     }
     
-    int firstPosition(int[] nums,int target){
-        int start = 0;
-        int end = nums.length-1;
-        while(start<=end){
+    int lastIndex(int[] nums,int target,int start,int end) {
+        
+        while(start <= end) {
+            
             int mid = start + (end - start)/2;
-            if(nums[mid] >= target) end = mid-1;
-            else  start = mid+1;
+            
+            if(nums[mid] <= target) start = mid + 1;
+            
+            else end = mid - 1;
         }
-        return start;
+        
+        if(end < 0 || end >= nums.length || nums[end] != target) end = -1;
+        
+        return end;
     }
     
-     int secondPosition(int[] nums,int target){
-        int start = 0;
-        int end = nums.length-1;
-        while(start<=end){
-             int mid = start + (end - start)/2;
-            if(nums[mid] > target) end = mid-1;
-            else  start = mid+1;
+    int startIndex(int[] nums,int target,int start,int end) {
+        
+        while(start <= end) {
+            
+            int mid = start + (end - start)/2;
+            
+            if(nums[mid] >= target) end = mid - 1;
+            else start = mid + 1;
         }
-        return end;
+        
+         if(start < 0 || start >= nums.length || nums[start] != target) start = -1;
+        return start;
     }
 }
