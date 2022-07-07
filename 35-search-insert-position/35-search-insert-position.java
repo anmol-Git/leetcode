6be1a22e0852fc7978444237b01c6fe416b3b7bb
anmol-Git <66984935+anmol-Git@users.noreply.github.com>
2022-07-07 14:36:11 +0530

    Time: 0 ms (100.00%), Space: 43.8 MB (28.26%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.java
+++ 35-search-insert-position/35-search-insert-position.java
@@ -1,13 +1,19 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length-1;
         
-        while(start<=end){
-            int mid = start +(end - start)/2;
-            if(nums[mid] >= target) end = mid-1;
-            else if(nums[mid] < target) start = mid +1;
+        int start = 0;
+        int end = nums.length - 1;
+        
+        if(target  > nums[end]) return nums.length;
+        
+        while(start < end) {
+            
+            int mid = start + (end - start)/2;
+            
+            if(nums[mid] < target) start = mid + 1;
+            else if (nums[mid] == target) return mid;
+            else end = mid;
         }
-        return end+1;
+        return end;
     }
 }
