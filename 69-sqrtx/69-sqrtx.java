653a20406cc827c91ef6fd52a6b96bb55c00ff56
anmol-Git <66984935+anmol-Git@users.noreply.github.com>
2022-07-08 13:45:45 +0530

    Time: 2 ms (82.70%), Space: 41.1 MB (71.95%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -1,15 +1,16 @@
 class Solution {
     public int mySqrt(int x) {
-        if(x==0 || x == 1) return x;
-        int start = 0;
-        int end = x;
+        if(x == 0 || x == 1) return x;
         
-        while(start<=end){
-            int mid = start + (end - start)/2;
-            if(mid > x/mid) end = mid-1;
-            else start = mid+1;
+        long start = 0;
+        long end = x;
+        
+        while(start <= end) {
+            long mid = start + (end - start)/2;
+            
+            if(mid * mid > x) end = mid - 1;
+            else start = mid + 1;
         }
-        
-    return end;
+        return (int)end;
     }
 }
