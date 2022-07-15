6aa285e0a350db527c4d49f3e11c8a2136318747
anmol-Git <66984935+anmol-Git@users.noreply.github.com>
2022-07-15 12:19:07 +0530

    Time: 1 ms (91.06%), Space: 43.6 MB (64.64%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -1,33 +1,31 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        if(nums.length <2) return;
-        int i = nums.length -2;
+        int len = nums.length;
+        if(len == 1) return;
         
-        while(i>=0 && nums[i+1] <= nums[i]){
-            i--;
-        }
-        int j = nums.length-1;
+        int i = len - 2;
         
-        if(i>=0){
-            while(j>i && nums[j] <= nums[i]){
-            j--;
-        }
-        swap(nums,i,j);
-        }
-        reverse(nums,i+1);
-    }
-    
-    void swap(int[]nums,int i,int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-    
-    void reverse(int[] nums,int start){
-        int end = nums.length-1;
+        while(i >= 0  && nums[i] >= nums[i + 1]) i--;
         
-        while(start<end){
-            swap(nums,start,end);
+        if(i >= 0) {
+            int j = len - 1;
+            
+            while(nums[j] <= nums[i]) j--;
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            
+        }
+        
+        //i + 1 to j reverse
+        
+        int start = i + 1;
+        int end = len - 1;
+        
+        while(start <= end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
             start++;
             end--;
         }
