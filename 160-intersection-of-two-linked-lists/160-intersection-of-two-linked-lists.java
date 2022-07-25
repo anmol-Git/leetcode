c751cf7952ecf1a2517fc0e377d926b381a479e0
anmol-Git <66984935+anmol-Git@users.noreply.github.com>
2022-07-25 10:56:50 +0530

    Time: 500 ms (5.59%), Space: 56.3 MB (6.69%) - LeetHub

---
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        
+        while(headA != null) {
+            
+            ListNode curr = headB;
+            
+            while(curr != null) {
+                
+                if(curr == headA) return headA;
+                
+                curr = curr.next;
+            }
+            
+            
+            headA = headA.next;
+            
+        }
+        
+        return null;
+    }
+}
