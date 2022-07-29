baeb13b46109a05ee3c6388c380d11af4ff2249d
anmol-Git <66984935+anmol-Git@users.noreply.github.com>
2022-07-29 11:50:35 +0530

    Time: 24 ms (39.74%), Space: 46.9 MB (16.62%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -6,22 +6,16 @@ class Solution {
         
         int[][] dp = new int[n + 1][m + 1];
         
-        for(int[] a : dp) Arrays.fill(a, -1);
-        
-        lcs(s1, s2, n, m, dp);
+        for(int i = 1; i <= n; i++) {
+            
+            for(int j = 1; j <= m; j++) {
+                
+                if(s1.charAt(i - 1) == s2.charAt(j - 1))  dp[i][j] = 1 + dp[i - 1][j - 1];
+                else dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                
+            }
+        }
         
         return dp[n][m];
-    }
-    
-    int lcs(String s1, String s2, int n, int m, int[][] dp) {
-        
-        if(dp[n][m] != -1) return dp[n][m];
-        
-        if(n == 0 || m == 0) return dp[n][m] = 0;
-        
-        if(s1.charAt(n - 1) == s2.charAt(m - 1)) return dp[n][m] = 1 + lcs(s1, s2, n - 1, m - 1, dp);
-        
-        return dp[n][m] = Math.max(lcs(s1, s2, n - 1, m, dp), lcs(s1, s2, n, m - 1, dp));
-        
     }
 }
