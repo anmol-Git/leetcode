2be395cedd2f3b18ef3cc2e9755b1cb99b136b2f
anmol-Git <66984935+anmol-Git@users.noreply.github.com>
2022-07-30 12:39:15 +0530

    Time: 3 ms (99.55%), Space: 43.3 MB (35.42%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.java
+++ 205-isomorphic-strings/205-isomorphic-strings.java
@@ -1,23 +1,28 @@
 class Solution {
-    public boolean isIsomorphic(String s, String t) {
-        
-        if(s.length() != t.length()) return false;
-        
-        Map<Character, Character> map = new HashMap<>();
-        Map<Character, Character> map2 = new HashMap<>();
-        
-        
-        for(int i = 0; i < s.length(); i++) {
-            
-            char c1 = s.charAt(i);
-            char c2 = t.charAt(i);
-            
-            if(!map.containsKey(c1)) map.put(c1,  c2);
-            if(!map2.containsKey(c2)) map2.put(c2, c1);
-            
-            if(map.get(c1) != c2 || map2.get(c2) != c1) return false;
+    
+    public boolean isIsomorphic(String sString, String tString) {
+
+        char[] s = sString.toCharArray();
+        char[] t = tString.toCharArray();
+
+        int length = s.length;
+        if(length != t.length) return false;
+
+        char[] sm = new char[256];
+        char[] tm = new char[256];
+
+        for(int i=0; i<length; i++){
+            char sc = s[i];
+            char tc = t[i];
+            if(sm[sc] == 0 && tm[tc] == 0){
+                sm[sc] = tc;
+                tm[tc] = sc;
+            }else{
+                if(sm[sc] != tc || tm[tc] != sc){
+                    return false;
+                }
+            }
         }
-        
         return true;
     }
 }
