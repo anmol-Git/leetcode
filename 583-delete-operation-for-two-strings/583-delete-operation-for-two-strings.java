5ef37c88bdc096fce6620e8c1356e9689cc965b2
anmol-Git <66984935+anmol-Git@users.noreply.github.com>
2022-08-01 09:45:25 +0530

    Time: 12 ms (79.10%), Space: 47.1 MB (35.05%) - LeetHub

---
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minDistance(String s1, String s2) {
+        
+        int n = s1.length();
+        int m = s2.length();
+        
+        int[][] dp = new int[n + 1][m + 1];
+        
+        for(int i = 1; i <= n; i++) {
+            
+            for(int j = 1; j <= m; j++) {
+                
+                if(s1.charAt(i - 1) == s2.charAt(j - 1)) dp[i][j] = 1 + dp[i - 1][j - 1];
+                else dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        
+        return (n - dp[n][m]) + (m - dp[n][m]);
+    }
+}
